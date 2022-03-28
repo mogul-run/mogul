0b8869e614e94f0a3f82a3d6af258dfaed55574f
Lucas  Song <lucas7song@gmail.com>
2022-03-28 14:33:06 -0700

    init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WIP from './pages/WIP';
+
+function App() {
+  return (
+    <div className="landing">
+      <WIP/>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/WIP.tsx
@@ -0,0 +1,11 @@
+import Chord from "../components/chord";
+
+function WIP() {
+    return (
+        <div className="wip-wrapper">
+            <Chord width={500} height={500} />
+        </div>
+    );
+}
+
+export default WIP;
