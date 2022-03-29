b9724b03ce0272d13f8bf4b9692065ef64b940fc
Lucas  Song <lucas7song@gmail.com>
2022-03-28 18:21:37 -0700

    add test sale page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import WIP from './pages/WIP';
+import React, { useEffect, useState } from "react";
+import {ethers} from "ethers";
+import { Routes, Route, Link } from "react-router-dom";
+import "./App.css";
+import WIP from "./pages/WIP";
+import TestSale from "./pages/test-sale";
 
 function App() {
-  return (
-    <div className="landing">
-      <WIP/>
-    </div>
-  );
+  const [userAccount, setUserAccount] = useState("");
+
+  const checkWalletIsConnected = () => { 
+    const { ethereum } = window as any;
+    if(!ethereum) {
+        console.log("wallet not connected!");
+    }
+    else {
+      connectWalletHandler();
+      console.log("wallet successfully connected!");
+    }
+  }
+
+  const connectWalletHandler = async () => { 
+    const { ethereum } = window as any;
+    if(!ethereum) {
+        alert("please install metamask");
+    }
+    else {
+        try {
+            const accounts = await ethereum.request({method: "eth_requestAccounts"});
+            console.log("Received accounts", accounts);
+            setUserAccount(accounts[0]);
+        }
+        catch (err) {
+          console.log("Error while retrieving account address: ", err);
+        }
+    }
+  }
+
+  // const mintNftHandler = async () => { 
+  //   try {
+  //     const { ethereum } = window as any;
+
+  //     if(ethereum) {
+  //       // provider, signer, then generate nftcontract 
+  //       const provider = new ethers.providers.Web3Provider(ethereum);
+  //       // provider uses private key to provide signer object to "sign" tx (access balance)
+  //       const signer = provider.getSigner();
+  //       const nftContract = new ethers.Contract(contractAddress, abi, signer);
+
+  //       console.log("init payment");
+  //       const nftTx = await nftContract.mintNFTs(1, {value: ethers.utils.parseEther("0.01")});
+
+  //       console.log("mining, please wait");
+  //       await nftTx.wait();
+
+  //       console.log(`Mined, please check transaction at https://ropsten.etherscan.io/tx/${nftTx.hash}`);
+  //     }
+  //     else {
+  //       alert("please install metamask");
+  //     }
+
+  //   }
+  //   catch(err) {
+  //       console.log("error while minting nft:", err);
+  //   }
+
+  // }
+
+  const connectWalletButton = () => {
+    return (
+      <button onClick={connectWalletHandler} className='cta-button connect-wallet-button'>
+        Connect Wallet
+      </button>
+    )
+  }
+
+  // const mintNftButton = () => {
+  //   return (
+  //     <button onClick={mintNftHandler} className='cta-button mint-nft-button'>
+  //       Mint NFT
+  //     </button>
+  //   )
+  // }
+
+  useEffect(() => {
+    checkWalletIsConnected();
+  }, [])
+
+    return (
+        <div className="app">
+            <Routes>
+                <Route path="/test-sale" element={<TestSale userAccount={userAccount}/>}/>
+                <Route path="/" element={<WIP/>}/>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
---
+++ frontend/src/pages/test-sale.tsx
@@ -0,0 +1,85 @@
+import { useEffect, useState } from "react";
+import "./test-sale.css";
+import { ethers } from "ethers";
+
+const crowdsaleAddr = "0xc30Ce1484CD5857fdAEDB321e05B7322D145Ec99";
+const tokenAddr = "0x8531f05D2F69E2591Dac5dFcaBc53b614fc636b4";
+const saleRatio = 1;
+
+function TestSale(props: any) {
+    const [numTokens, setNumTokens] = useState(0);
+    const [tokenBalance, setTokenBalance] = useState(0);
+
+    useEffect( () => {
+        getTokenBalance();
+    })
+
+    const getTokenBalance = async () => {
+        try {
+            const { ethereum } = window as any;
+            if (ethereum) {
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const hex_balance = await provider.getBalance(
+                   tokenAddr 
+                );
+                setTokenBalance(ethers.utils.formatEther(hex_balance));
+            } else {
+                alert("please install metamask");
+            }
+        } catch (err) {
+            console.log("error while retrieving balance", err);
+        } 
+    }
+    const updateInputValue = (event: any) => {
+        setNumTokens(event.target.value);
+    };
+
+    const handlePurchase = async () => {
+        try {
+            const { ethereum } = window as any;
+
+            if (ethereum) {
+                // provider, signer, then generate nftcontract
+                const provider = new ethers.providers.Web3Provider(ethereum);
+                const params = [{
+                    from: props.userAccount,
+                    to: crowdsaleAddr,
+                    value: ethers.utils.parseUnits(String(numTokens), 'matic').toHexString()
+                }];
+            
+                const transactionHash = await provider.send('eth_sendTransaction', params)
+                console.log('transactionHash is ' + transactionHash);
+                // provider uses private key to provide signer object to "sign" tx (access balance)
+            } else {
+                alert("please install metamask");
+            }
+        } catch (err) {
+            console.log("error while handling purchase:", err);
+        }
+    };
+    return (
+        <div className="test-sale-wrapper">
+            <h1>Token Sale for $LUCAS</h1>
+            <div>Token address: 0xc30Ce1484CD5857fdAEDB321e05B7322D145Ec99</div>
+            <div> Total $LUCAS that can be sold {tokenBalance}</div>
+            <br />
+            <div>
+                Number of $LUCAS to buy:
+                <div>
+                    <input
+                        value={numTokens}
+                        onChange={(event) => updateInputValue(event)}
+                    />
+                    $LUCAS
+                </div>
+                <br />
+                Total cost: {numTokens * saleRatio} $MATIC
+                <div>
+                    <button onClick={() => handlePurchase()}> purchase </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default TestSale;
