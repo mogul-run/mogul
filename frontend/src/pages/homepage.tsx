c3bdab4f38377b397999cbd166249059207082b4
Lucas  Song <lucas7song@gmail.com>
2022-04-04 17:06:32 -0700

    add second panel

--- frontend/src/pages/homepage.tsx
+++ frontend/src/pages/homepage.tsx
@@ -8,32 +8,35 @@ function HomePage() {
                 <div className="logo">Mogul</div>
                 <div className="navbuttons">
                     <div className="navlinks">
-                        <div className="navlink">link1</div>
-                        <div className="navlink">link2</div>
-                        <div className="navlink">link3</div>
+                        <div className="navlink">about</div>
+                        <div className="navlink">discord</div>
+                        <div className="navlink">????</div>
                     </div>
                     <div className="connect">connect</div>
                 </div>
             </div>
             <div className="content">
-                <HomePageContent />
+                <ContentUno />
+                <ContentDos />
+                <ContentTres />
             </div>
         </div>
     );
 }
 
-function HomePageContent() {
+function ContentUno() {
     return (
         <div className="content-container">
             <div className="content-left">
                 {" "}
-                <div className="content-header">
+                <div className="content-header header-block">
                     Bringing the bum out of everyone
                 </div>
                 <div className="content-desc">
                     <div className="content-block">
-                        Living the bum life is a good life. We're trying to help more people 
-                        pursue the things they want to do in life and make a living doing so.
+                        Living the bum life is a good life. We're trying to help
+                        more people pursue the things they want to do in life
+                        and make a living doing so.
                     </div>
                     <div className="content-block">
                         Powered by public smart contracts on{" "}
@@ -56,6 +59,75 @@ function HomePageContent() {
                     <img className="stonemasters" src={Stonemasters} />
                 </div>
             </div>
+        </div>
+    );
+}
+
+// this block will provide basic content of what the platform will stand for
+function ContentDos() {
+    return (
+        <div className="content-container dos">
+            <div className="definition-header">
+                ✍️ It's time to learn some new words!
+            </div>
+            <div className="definition-container">
+                <div className="content-left">
+                    <div className="definition-word">
+                        <b> Bum </b> <i> noun</i>:
+                    </div>
+                </div>
+                <div className="content-right">
+                    <span className="definition">
+                        1. a person who devotes a great deal of time to a
+                        recreational activity.
+                        <br />
+                        2. to spend time unemployed and often wandering
+                    </span>
+                </div>
+            </div>
+            <div className="definition-container">
+                <div className="content-left">
+                    <div className="definition-word">
+                        <b> Bumsport </b> <i> noun</i>:
+                    </div>
+                </div>
+                <div className="content-right">
+                    <span className="definition">
+                        Any sport in which a significant percentage of
+                        practitioners are bums of definition (1.)
+                    </span>
+                </div>
+            </div>
+            <div>
+                <div className="blurb">
+                    <div>
+                        <i>
+                            {" "}
+                            <b> Let's practice using these words... </b>{" "}
+                        </i>
+                        <br />
+                        <br />
+                    </div>
+                    <span className="highlight">Bums</span> across history have tapped into intimate communities to further their skills.  
+                    <br />
+                    <br />
+                    <span className="highlight">Bumsports</span> have long been
+                    overshadowed by conventional sports (think 
+                    basketball, football).
+                    <br />
+                    <br />
+                    <span className="highlight">Bumsports</span> are experiencing unprecedented growth and popularity. 
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function ContentTres() {
+    return (
+        <div className="content-container">
+            <div className="content-left">picture</div>
+            <div className="content-right">content</div>
         </div>
     );
 }
