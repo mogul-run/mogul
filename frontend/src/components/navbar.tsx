8686ccc64ecae7b937ef8ffecb682759a972ab8d
Lucas  Song <lucas7song@gmail.com>
2022-04-06 10:17:22 -0700

    add tgob content

--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,31 +1,34 @@
 import { Link } from "react-router-dom";
 import "./navbar.css";
 
-export default function Navbar(props:any) {
+export default function Navbar(props: any) {
     return (
-            <div className="navbar">
+        <div className="navbar">
+            <Link to="/" className="nostyle">
                 <div className="logo">Mogul</div>
-                <div className="navbuttons">
-                    <div className="navlinks">
-                        {/* <div className="navlink" onClick={scrollToContent}>About</div> */}
-                        <a
-                            href="https://discord.gg/8AXyshRRVM"
-                            target="__blank"
-                            className="nostyle"
-                        >
-                            <div className="navlink"> Discord</div>
-                        </a>
+            </Link>
+            <div className="navbuttons">
+                <div className="navlinks">
+                    {/* <div className="navlink" onClick={scrollToContent}>About</div> */}
+                    <a
+                        href="https://discord.gg/8AXyshRRVM"
+                        target="__blank"
+                        className="nostyle"
+                    >
+                        <div className="navlink"> Discord</div>
+                    </a>
 
-                        <Link
-                            to="/the-game-of-bum"
-                            className="nostyle"
-                        >
+                    <Link to="/the-game-of-bum" className="nostyle">
                         <div className="navlink">T.G.o.B</div>
-                        </Link>
-                    </div>
-                    <div className="connect" onClick={() => props.connectWalletHandler()}>connect</div>
+                    </Link>
+                </div>
+                <div
+                    className="connect"
+                    onClick={() => props.connectWalletHandler()}
+                >
+                    connect
                 </div>
             </div>
-    )
-
+        </div>
+    );
 }
--- frontend/src/pages/TGOB.tsx
+++ frontend/src/pages/TGOB.tsx
@@ -1,7 +1,55 @@
-
+import "./TGOB.css";
+import "./homepage.css";
 
 function TGOB() {
-    return <div>The Game of Bum</div>;
+    return (
+        <div className="TGOB">
+            <div className="head-wrapper">
+                <div className="head">
+                    <div className="title">
+                        <a
+                            className="bum-link"
+                            href="https://www.skimag.com/videos/gnar-movie-0/"
+                            target="__blank"
+                        >
+                            The Game of
+                            <div className="title-big">B.U.M</div>
+                        </a>
+                    </div>
+                </div>
+                <div className="subtext">
+                    <i>🦅 Dedicated to Shane McConkey and Brad Gobright 🦅</i>
+                </div>
+            </div>
+            <div className="info-wrapper">
+                <div className="desc-wrapper">
+                    <div className="header">Description</div>
+                    <div className="desc">
+                        The Game of B.U.M (T.G.o.B) is a competition for bums
+                        across the world to showcase their mastery of bumsports.{" "}
+                    </div>
+                </div>
+                <div className="desc-wrapper">
+                    <div className="header">Rules</div>
+                    <div className="desc">
+                        The Game of B.U.M (T.G.o.B) is a competition for bums
+                        across the world to showcase their mastery of bumsports.{" "}
+                    </div>
+                </div>
+            </div>
+            <div className="desc-wrapper">
+                <div className="header">Prizes</div>
+                <div className="desc">
+                    <ul>
+                        <li>Bragging Rights</li>
+                        <li>Tokens</li>
+                        <li>Gear from our sponsors</li>
+                        <li>Exclusive access to the Mogul Platform.</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default TGOB;
