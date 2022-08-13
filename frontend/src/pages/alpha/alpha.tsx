45249777e04c6b85ea7278f4fa6120f6ee16020e
Lucas  Song <lucas7song@gmail.com>
2022-08-13 02:02:28 -0700

    add description

--- frontend/src/pages/alpha/alpha.tsx
+++ frontend/src/pages/alpha/alpha.tsx
@@ -1,19 +1,33 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { useAuth } from "../../context/authContext";
 import { useAuthModal } from "../../context/modalContext";
 import Cards from "./cards";
 
-function Series1() {
+// to do: add sign up for rsvp list
+// to do: add nft entries in db
+
+function Alpha() {
     const { nft_id } = useParams();
     const isMobile = window.innerWidth < 768;
     const { handleModal } = useAuthModal();
     const { getUser } = useAuth();
+    let navigate = useNavigate();
 
     const drawing_width = isMobile ? 350 : 550;
 
     const handleLMK = () => {
         if (getUser()) {
-            console.log(getUser())
+            // add user to rsvp list
+
+            console.log(getUser());
+        } else {
+            handleModal(true);
+        }
+    };
+    const handleWant = () => {
+        if (getUser()) {
+            // add user to rsvp list
+            console.log(getUser());
         } else {
             handleModal(true);
         }
@@ -36,31 +50,56 @@ function Series1() {
                                     <div className="text-2xl font-bold cursor-pointer">
                                         ???
                                     </div>
-                                    <div className="text-md">
+                                    <div className="text-xs">
                                         details for this nft have not been
                                         revealed.
                                     </div>
                                 </div>
-                                <div>
+                                <div className="flex flex-col content-start">
                                     <div
                                         className="btn-ghost cursor-pointer"
-                                        onClick={() => handleLMK()}
+                                        onClick={() => handleWant()}
                                     >
-                                        lmk when?
+                                        i want
+                                    </div>
+                                    <div
+                                        className="text-xs text-orange-600 mx-2 cursor-pointer"
+                                        onClick={() => navigate(`/alpha/`)}
+                                    >
+                                        bring me home
                                     </div>
                                 </div>
                             </div>
                         </div>
                     ) : (
                         // <div className={`w-[${drawing_width}px]`}>
-                        <div>
-                            <div className="text-6xl bold mb-2">α</div>
-                            <div className="text-xs small ">
-                                our debut series of 10 NFTs. each nft entitles
-                                you to a different real world experience, and
-                                provides proof of participation after redeemed.
-                                minting nfts are on a first come first serve
-                                basis, and nft distribution will be random.
+                        <div className="flex space-y-2 items-center">
+                            <div>
+                                <div className="text-6xl bold mb-2">α</div>
+                                <div className="text-xs">
+                                    our debut series of 10 nfts. each nft
+                                    entitles you to a guided outdoor 
+                                    experience: surfing lessons in santa
+                                    cruz, bouldering at mortar rock,
+                                    biking around san francisco and more. experiences
+                                    come with equipment provided and curated food and beverage
+                                    pairings.
+                                    <br/>
+                                    <br/>
+                                     minting nfts are on a first come
+                                    first serve basis, and minting entitles you
+                                    to one randomly selected nft. nfts can be
+                                    transferred or sold after minting, but can only be
+                                    redeemed once.
+                                </div>
+                            </div>
+                            <div>
+                                <div
+                                    className="btn-ghost font-bold cursor-pointer"
+                                    onClick={() => handleLMK()}
+                                >
+                                    lmk when
+                                </div>
                             </div>
                         </div>
                     )}
@@ -73,4 +112,4 @@ function Series1() {
     );
 }
 
-export default Series1;
+export default Alpha;
