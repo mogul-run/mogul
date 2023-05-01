5f7e7670aea1033a9618f20a187782b536890a98
Lucas  Song <lucas7song@gmail.com>
2023-05-01 16:08:16 -0700

    continue to submodule this repo

--- frontend/src/labs/landing.tsx
+++ frontend/src/labs/landing.tsx
@@ -1,8 +1,11 @@
 function Landing() {
-    return (<div>
-        mogul run labs
-        est 2021
-    </div>  );
+    return (
+        <div className="w-100 h-100">
+            <div className="w-20 h-30 items-center justify-center">
+                mogul run labs est 2021
+            </div>
+        </div>
+    );
 }
 
 export default Landing;
