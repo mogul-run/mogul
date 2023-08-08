690cd4bb1662366453bb40ca08098498023a8a0a
Lucas  Song <lucas7song@gmail.com>
2023-08-08 11:55:14 -0700

    homepage styling

--- frontend/src/labs/landing.tsx
+++ frontend/src/labs/landing.tsx
@@ -4,21 +4,27 @@ function Landing() {
     return (
         <div className="w-100 h-screen flex items-center justify-center landing-background">
             <div className="flex flex-col w-96 h-96 gap-4 items-center justify-center rounded-full backdrop-grayscale backdrop-blur-sm">
-                <div className="text-2xl font-extrabold text-stone-300">
-                    mogul run labs 
-                <div className="text-xs font-light text-stone-300">
-                    est. 2021
+                <div className="text-4xl text-orange-500">
+                    <span className="font-extrabold text-6xl">mogul</span>
+                    <span className="font-extra-light font-sans">.run</span>
+                    <div className="font-serif font-8xl text-center ml-8">labs</div>
+                    <div className="text-xs text-orange-600 text-center">est. 2021</div>
                 </div>
-                </div>
-                <ul className="flex flex-col gap-2">
+                <ul className="flex flex-col gap-2 text-purple-600">
                     <li>
-                        <a href="https://communities.mogul.run"> communities </a>
+                        <a
+                            href="https://communities.mogul.run"
+                            className="text-purple-500"
+                        >
+                            {" "}
+                            communities{" "}
+                        </a>
                     </li>
                     <li>
-                        <a href="https://kits.mogul.run"> kits </a>
+                        <a href="https://kits.mogul.run" className="text-orange-600"> kits </a>
                     </li>
                     <li>
-                        <a href="https://blog.mogul.run"> blog </a>
+                        <a href="https://blog.mogul.run" className="text-purple-300"> blog </a>
                     </li>
                 </ul>
             </div>
